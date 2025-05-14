1e3dbb97e22b703ea353c79b98934a9ebf12cb87
JaiBhargav <bhargav.cloud01@gmail.com>
2025-05-14 13:02:52 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -15,9 +15,9 @@ resource "aws_s3_bucket_ownership_controls" "example" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.mybucket.id
   block_public_acls       = true
-  block_public_policy     = false
+  block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = false
+  restrict_public_buckets = true
 }
 
 # Add public read policy for website hosting
