f3a51637e78d69e281d7123c993c60024a125290
JaiBhargav <bhargav.cloud01@gmail.com>
2025-05-14 12:20:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "BhargavJupalli-application-buckett"
+  default     = "bhargavjupalli-application-buckett"
 }
 
 variable "acl" {
